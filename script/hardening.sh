14b53586976de58d7ef169e180b6699df02085a5
Etienne-Chamarier <119299614+Etienne-Chamarier@users.noreply.github.com>
2024-01-31 11:14:23 +0100

    Update hardening.sh

--- script/hardening.sh
+++ script/hardening.sh
@@ -23,6 +23,11 @@ echo "Création du fichier hardening dans /etc/ssh/sshd_config.d..."
 sudo touch /etc/ssh/sshd_config.d/hardening.conf
 sudo chown root:root /etc/ssh/sshd_config.d/hardening.conf
 sudo chmod 644 /etc/ssh/sshd_config.d/hardening.conf
+
+
+# Nettoyer le fichier de configuration pour ne pas avoir de doublon si le script est lancer plusieurs fois.
+sudo sed -i '/PermitRootLogin\|MaxAuthTries\|LoginGraceTime\|PermitEmptyPasswords\|PasswordAuthentication\|ChallengeResponseAuthentication\|KerberosAuthentication\|X11Forwarding\|PermitUserEnvironment\|DebianBanner/d' /etc/ssh/sshd_config.d/hardening.conf
+
 sudo sshd -t
 
 # Configuration du fichier hardening.conf
